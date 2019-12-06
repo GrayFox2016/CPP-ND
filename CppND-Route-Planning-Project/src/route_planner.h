72d6213972375e6f4d2986a810f3a67e9b399ef8
GrayFox <87567639@qq.com>
2019-12-06 09:09:39 +0000

    finished

---
+++ CppND-Route-Planning-Project/src/route_planner.h
@@ -0,0 +1,41 @@
+#ifndef ROUTE_PLANNER_H
+#define ROUTE_PLANNER_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "route_model.h"
+#include <algorithm>
+
+
+class RoutePlanner {
+  public:
+    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
+    // Add public variables or methods declarations here.
+    float GetDistance() const {return distance;}
+    void AStarSearch();
+
+    // The following methods have been made public so we can test them individually.
+    void AddNeighbors(RouteModel::Node *current_node);
+    float CalculateHValue(RouteModel::Node const *node);
+    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
+    RouteModel::Node *NextNode();
+
+    // Addtional methods
+    static bool Compare(const RouteModel::Node *a, const RouteModel::Node *b) {
+      float f_a = a->g_value + a->h_value;
+      float f_b = b->g_value + b->h_value;
+      return f_a > f_b;
+    };
+
+  private:
+    // Add private variables or methods declarations here.
+    std::vector<RouteModel::Node*> open_list;
+    RouteModel::Node *start_node;
+    RouteModel::Node *end_node;
+
+    float distance = 0.0f;
+    RouteModel &m_Model;
+};
+
+#endif
